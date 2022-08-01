68dcd2198b5a077a5b8cedeb141d7fddd7db4649
Abdul Karim Farooqi <Abdul Karim Farooqi@10.0.0.4>
2022-08-01 19:13:59 -0400

    Implement commite

--- src/gitDay2/GidDay2.java
+++ src/gitDay2/GidDay2.java
@@ -4,6 +4,10 @@ public class GidDay2 {
 public static void main(String[] args) {
 	
 	System.out.println("GitDay2");
+}
+	public static void add(int a, int b) {
+		System.out.println(a+b);
+	}
 	
 }
-}
+
