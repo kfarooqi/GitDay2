c7e842a9be7f4b9a33fdea71f706102beff363e9
kfarooqi <96351700+kfarooqi@users.noreply.github.com>
2022-08-01 20:08:39 -0400

    Update GidDay2.java

--- src/gitDay2/GidDay2.java
+++ src/gitDay2/GidDay2.java
@@ -13,7 +13,7 @@ public static void main(String[] args) {
 		System.out.println(c+d);
 	}
 	
-	String str = "Hello";
-	System.out.println(str);
+	//String str = "Hello";
+	//System.out.println(str);
 }
 
